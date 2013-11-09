5c97b90e9799534e3e19c076941eed920c254d9f
rejuvyesh <rejuvyesh@gmail.com>
2013-11-09 23:12:05 +0530

    add markdown customization for smartparens

--- init.el
+++ init.el
@@ -205,6 +205,17 @@ Usage: (package-require 'package)"
 ;; (require 'autopair)
 ;; (autopair-global-mode) ;; to enable in all buffers
 (require 'smartparens-config)
+(smartparens-global-mode t)
+;;; markdown-mode
+(sp-with-modes '(markdown-mode gfm-mode rst-mode)
+               (sp-local-pair "*" "*" :bind "C-*")
+               (sp-local-tag "2" "**" "**")
+               (sp-local-tag "s" "```scheme" "```")
+               (sp-local-tag "<"  "<_>" "</_>" :transform 'sp-match-sgml-tags))
+;;; html-mode
+(sp-with-modes '(html-mode sgml-mode)
+               (sp-local-pair "<" ">"))
+
 
 ;; auto completion
 (require 'auto-complete-config)
