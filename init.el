1188c97c35307aaaebf834290016304dbcb4d7cd
rejuvyesh <rejuvyesh@gmail.com>
2013-11-10 00:02:36 +0530

    flesh out custom file

--- init.el
+++ init.el
@@ -50,6 +50,10 @@ Usage: (package-require 'package)"
 ;;       (normal-top-level-add-subdirs-to-load-path))
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
+;; Keep emacs Custom-settings in separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
 ; try to keep windows within a max margin
 (require 'automargin)
 (setq automargin-target-width 120)
@@ -267,27 +271,6 @@ Usage: (package-require 'package)"
       (find-file file))))
 (global-set-key "\C-x\C-r" 'recentf-ido-find-file)
 ;; stop tramp file competions
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" "a0feb1322de9e26a4d209d1cfa236deaf64662bb604fa513cca6a057ddf0ef64" "0a47a318b366c8d5bf2a4738ff4cea9988c60f4b3b7f7a31cff565a7889406a5" "88d556f828e4ec17ac074077ef9dcaa36a59dccbaa6f2de553d6528b4df79cbd" "2282f550e7baf0ff8eb1432983676d20fed81a2fdd8c50d70e62cb58580633f4" "843a82ff3b91bec5430f9acdd11de03fc0f7874b15c1b6fbb965116b4c7bf830" "5e2ade7f65d9162ca2ba806908049fb37d602d59d90dc3a08463e1a042f177ae" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "bf7ed640479049f1d74319ed004a9821072c1d9331bc1147e01d22748c18ebdf" default)))
- '(ecb-fix-window-size (quote auto))
- '(ecb-layout-name "left15")
- '(ecb-layout-window-sizes (quote (("left14" (ecb-speedbar-buffer-name 33 . 45) (ecb-history-buffer-name 33 . 23)) ("left15" (ecb-speedbar-buffer-name 33 . 34) (ecb-methods-buffer-name 33 . 34)))))
- '(ecb-options-version "2.40")
- '(ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2))
- '(ecb-process-non-semantic-files t)
- '(ecb-tip-of-the-day nil)
- '(ecb-toggle-layout-sequence (quote ("left14" "left15")))
- '(ecb-use-speedbar-instead-native-tree-buffer (quote dir))
- '(ecb-window-width 33)
- '(evernote-developer-token "S=s162:U=117b1be:E=1496c388c33:C=14214876037:P=1cd:A=en-devtoken:V=2:H=b31dbef3ff74c8b74ff117549bd396af")
- '(ido-enable-tramp-completion nil)
- '(matlab-shell-command-switches (quote ("-nodesktop -nosplash")))
- '(linum-format " %7i ")
- '(safe-local-variable-values nil))
 
 ;; disable tramp
 (setq tramp-mode nil)
@@ -311,8 +294,6 @@ Usage: (package-require 'package)"
 ;; power line
 (require 'powerline)
 (setq powerline-arrow-shape 'arrow14)
-
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -911,7 +892,7 @@ If visual-line-mode is on, then also jump to beginning of real line."
 
 ;; Guide Key
 (require 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x 8"))
+(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x 8" "C-x +"))
 (guide-key-mode 1)
 (setq guide-key/recursive-key-sequence-flag t)
 (setq guide-key/popup-window-position 'bottom)
