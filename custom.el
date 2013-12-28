b134d13862d351ac74705a49b87974d19743791a
rejuvyesh <mail@rejuvyesh.com>
2013-12-28 10:33:49 +0530

    custom faces

--- custom.el
+++ custom.el
@@ -28,4 +28,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(diff-added ((((background dark)) (:foreground "#FFFF9B9BFFFF")) (t (:foreground "DarkGreen"))) t)
+ '(diff-changed ((((background dark)) (:foreground "Yellow")) (t (:foreground "MediumBlue"))) t)
+ '(diff-context ((((background dark)) (:foreground "White")) (t (:foreground "Black"))) t)
+ '(diff-file-header ((((background dark)) (:foreground "Cyan" :background "Black")) (t (:foreground "Red" :background "White"))) t)
+ '(diff-header ((((background dark)) (:foreground "Cyan")) (t (:foreground "Red"))) t)
+ '(diff-hunk-header ((((background dark)) (:foreground "Black" :background "#05057F7F8D8D")) (t (:foreground "White" :background "Salmon"))) t)
+ '(diff-index ((((background dark)) (:foreground "Magenta")) (t (:foreground "Green"))) t)
+ '(diff-nonexistent ((((background dark)) (:foreground "#FFFFFFFF7474")) (t (:foreground "DarkBlue"))) t)
+ '(diff-removed ((((background dark)) (:foreground "#7474FFFF7474")) (t (:foreground "DarkMagenta"))) t))
