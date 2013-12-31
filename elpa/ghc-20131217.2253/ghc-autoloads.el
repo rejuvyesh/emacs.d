6d0d810fcdf6937d47c2b414e359d64fb741abc8
rejuvyesh <mail@rejuvyesh.com>
2013-12-31 14:49:36 +0530

    ghc-mod

---
+++ elpa/ghc-20131217.2253/ghc-autoloads.el
@@ -0,0 +1,29 @@
+;;; ghc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (ghc-init) "ghc" "ghc.el" (21185 23308 315687 796000))
+;;; Generated autoloads from ghc.el
+
+(autoload 'ghc-init "ghc" "\
+
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ghc-command.el" "ghc-comp.el" "ghc-doc.el"
+;;;;;;  "ghc-flymake.el" "ghc-func.el" "ghc-indent.el" "ghc-info.el"
+;;;;;;  "ghc-ins-mod.el" "ghc-pkg.el") (21185 23308 682200 612000))
+
+;;;***
+
+(provide 'ghc-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ghc-autoloads.el ends here
---
+++ elpa/ghc-20131217.2253/ghc-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ghc" "20131217.2253" "Sub mode for Haskell mode" 'nil)
