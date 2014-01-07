a0c23e9a98cb298d48a81b8c62813e3940d85681
rejuvyesh <mail@rejuvyesh.com>
2014-01-07 20:25:07 +0530

    insert haskell function type

--- init.el
+++ init.el
@@ -602,6 +602,44 @@ See the variable `align-rules-list' for more details.")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (when (eval-when-compile (>= emacs-major-version 24))
   (add-hook 'inferior-haskell-mode-hook 'turn-on-ghci-completion))
+(defun haskell-insert-type ()
+  "Insert the type of the function on the previous line.
+You have to be be on the first line of the function.
+Use `haskell-insert-type-infix' for infix functions.
+It uses the Haskell inferior process in order to get the class constraints
+as well"
+  (interactive)
+  (save-excursion
+    (beginning-of-line)
+    (zap-up-to-char 1 ?\s)
+    (yank)
+    (let ((func-type (inferior-haskell-type (yank-peek))))
+      (beginning-of-line)
+      (newline)
+      (previous-line)
+      (insert func-type))))
+
+(defun haskell-insert-type-infix ()
+  "Insert a type of the infix function on the previous line.
+Also see `haskell-insert-type'. Note that this works by using the spaces between
+arguments to determine the of function so it will fail if your first argument
+contains a space, for example when matching on a String.
+It uses the Haskell inferior process in order to get the class constraints
+as well. Make sure you have loaded the file using `inferior-haskell-load-file'."
+  (interactive)
+  (save-excursion
+    (beginning-of-line)
+    (zap-up-to-char 1 ?\s)
+    (yank)
+    (forward-char)
+    (zap-up-to-char 1 ?\s)
+    (yank)
+    (let ((func-type (inferior-haskell-type
+                      (concat "(" (yank-peek) ")"))))
+      (beginning-of-line)
+      (newline)
+      (previous-line)
+      (insert func-type))))
 
 ;; org-mode
 (setq org-special-ctrl-a/e t)
