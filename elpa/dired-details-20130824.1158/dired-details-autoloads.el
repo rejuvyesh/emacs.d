d9d2d9592956416385220aacdf773c1d165bdaa5
rejuvyesh <mail@rejuvyesh.com>
2014-04-05 11:23:17 +0530

    reinstall certain packages

---
+++ elpa/dired-details-20130824.1158/dired-details-autoloads.el
@@ -0,0 +1,18 @@
+;;; dired-details-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("dired-details-pkg.el" "dired-details.el")
+;;;;;;  (21311 39208 236183 597000))
+
+;;;***
+
+(provide 'dired-details-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dired-details-autoloads.el ends here
---
+++ elpa/go-autocomplete-20140317.1911/go-autocomplete-autoloads.el
@@ -0,0 +1,18 @@
+;;; go-autocomplete-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("go-autocomplete-pkg.el" "go-autocomplete.el")
+;;;;;;  (21311 39412 579740 927000))
+
+;;;***
+
+(provide 'go-autocomplete-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; go-autocomplete-autoloads.el ends here
