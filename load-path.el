8fdf18bdfec229d9b4e6200e8d31d9d6b652129d
rejuvyesh <mail@rejuvyesh.com>
2014-04-07 20:33:01 +0530

    load-path separated

---
+++ load-path.el
@@ -0,0 +1,21 @@
+;; local stores manually maintained packages
+(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+    (let* ((my-lisp-dir "~/.emacs.d/local/")
+           (default-directory my-lisp-dir))
+      (progn
+        (setq load-path (cons my-lisp-dir load-path))
+        (normal-top-level-add-subdirs-to-load-path))))
+(setq load-path (cons (expand-file-name "~/.emacs.d") load-path))
+
+;; package-repositories
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+;; some generic aliases that make elisp less painful
+(require 'cl-lib)
+(defalias 'first 'cl-first)
+(defalias 'second 'cl-second)
+
+(provide 'load-path)
