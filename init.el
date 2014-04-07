82bebb31315a7c9ab287b3d155c561722b83008f
rejuvyesh <mail@rejuvyesh.com>
2014-04-07 20:22:51 +0530

    clean up init

--- init.el
+++ init.el
@@ -5,28 +5,16 @@
 (setq user-full-name "Jayesh Kumar Gupta"
       user-mail-address "mail@rejuvyesh.com")
 
-;; site-lisp stores manually maintained packages
-(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-    (let* ((my-lisp-dir "~/.emacs.d/local/")
-           (default-directory my-lisp-dir))
-      (progn
-        (setq load-path (cons my-lisp-dir load-path))
-        (normal-top-level-add-subdirs-to-load-path))))
-(setq load-path (cons (expand-file-name "~/.emacs.d") load-path))
-
-;; package-repositories
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
+;; load path (the only hard-coded path, so we can use the file in external scripts without duplicating where load-paths are defined)
+(load "~/.emacs.d/load-path.el")
 
 ;; init setup
-(require 'cl-lib)
 (require 'setup)
 (setup-initialize)
 
 ;; Keep emacs Custom-settings in separate file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
 ;;(load custom-file)
 (setup-include "custom")
 
@@ -53,43 +41,4 @@
 (setup "setup-major-modes")
 
 ;; others
-
-(add-hook 'after-save-hook
-          (lambda()
-            (let ((file (buffer-file-name)))
-              (if (and file (string-match "init\\.el$" file))
-                  (byte-compile-file file)))))
-
-(setq gc-cons-threshold 20000000)
-
-;; use y/n instead of yes/no
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; Guide Key
-(setup "guide-key"
-       (setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x 8" "C-x +" "C-x c"))
-       (guide-key-mode 1)
-       (setq guide-key/recursive-key-sequence-flag t)
-       (setq guide-key/popup-window-position 'bottom))
-
-;; make zsh aliases work
-(setq shell-command-switch "-lc")
-
-;; diminish
-(setup "diminish"
-  (setup-after "anzu" (diminish 'anzu-mode))
-  (setup-after "auto-complete"(diminish 'auto-complete-mode "↝"))
-  (setup-after "auto-revert-mode" (diminish 'auto-revert-mode))
-  (setup-after "eldoc" (diminish 'eldoc-mode))
-  (setup-after "fic-mode" (diminish 'fic-mode))
-  (setup-after "guide-key" (diminish 'guide-key-mode))
-  (setup-after "haskell-doc" (diminish 'haskell-doc-mode))
-  (setup-after "haskell-indentation" (diminish 'haskell-indentation-mode))
-  (setup-after "highlight-parentheses" (diminish 'highlight-parentheses-mode))
-  (setup-after "hs-minor-mode" (diminish 'hs-minor-mode))
-  (setup-after "smartparens-autoloads" (diminish 'smartparens-mode))
-  (setup-after "undo-tree" (diminish 'undo-tree-mode "↺"))
-  (setup-after "visual-line-mode" (diminish 'visual-line-mode))
-  (setup-after "volatile-highlights" (diminish 'volatile-highlights-mode))
-  (setup-after "whole-line-or-region" (diminish 'whole-line-or-region-mode))
-  (setup-after "yasnippet" (diminish 'yas-minor-mode)))
+(setup "setup-misc")
