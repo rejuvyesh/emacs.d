b55bd49497bddb503fb9a74e23ad096228ab57a0
rejuvyesh <mail@rejuvyesh.com>
2014-04-08 01:39:40 +0530

    try auto compile again

--- setup-major-modes.el
+++ setup-major-modes.el
@@ -255,6 +255,15 @@
 (setup-after "lisp-mode"
   (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode))
 
+;; emacs-lisp
+(defun byte-compile-current-buffer ()
+  "`byte-compile' current buffer if it's emacs-lisp-mode and compiled file exists."
+  (interactive)
+  (when (and (eq major-mode 'emacs-lisp-mode)
+             (file-exists-p (byte-compile-dest-file buffer-file-name)))
+    (byte-compile-file buffer-file-name)))
+(add-hook 'after-save-hook 'byte-compile-current-buffer)
+
 ;; go
 (setup "go-mode"
   (add-to-list 'auto-mode-alist '("\\.go$" . go-mode))
