f771eaefa648286c7b16c7a433fe498a77c467c1
rejuvyesh <mail@rejuvyesh.com>
2014-04-08 23:38:39 +0530

    new packages

---
+++ elpa/magit-log-edit-20140303.700/magit-log-edit-autoloads.el
@@ -0,0 +1,18 @@
+;;; magit-log-edit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("magit-log-edit-pkg.el" "magit-log-edit.el")
+;;;;;;  (21316 14530 622401 602000))
+
+;;;***
+
+(provide 'magit-log-edit-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; magit-log-edit-autoloads.el ends here
