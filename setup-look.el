f4405fde31d8c6b2956547eae0fad451b91db7f1
rejuvyesh <mail@rejuvyesh.com>
2014-04-11 11:16:48 +0530

    try out leuven theme

--- setup-look.el
+++ setup-look.el
@@ -19,11 +19,11 @@
 ;; Remove background color from terminal emacs,
 ;; so that it can remain transparent
 ;; http://stackoverflow.com/questions/19054228/emacs-disable-theme-background-color-in-terminal
-(defun on-frame-open (frame)
-  (if (not (display-graphic-p frame))
-      (set-face-background 'default "unspecified-bg" frame)))
-(on-frame-open (selected-frame))
-(add-hook 'after-make-frame-functions 'on-frame-open)
+;; (defun on-frame-open (frame)
+;;   (if (not (display-graphic-p frame))
+;;       (set-face-background 'default "unspecified-bg" frame)))
+;; (on-frame-open (selected-frame))
+;; (add-hook 'after-make-frame-functions 'on-frame-open)
 
 (defvar bright-theme 'leuven  "Bright theme to use")
 (defvar dark-theme   'molokai "Dark theme to use")
