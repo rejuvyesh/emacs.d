b30f848abde932d7efcc7948bb30f238b21c2a83
rejuvyesh <mail@rejuvyesh.com>
2014-04-12 11:38:44 +0530

    more comments

--- local/keyboard-cat-mode.el
+++ local/keyboard-cat-mode.el
@@ -6,16 +6,23 @@
 
 ;;; Code:
 
-(defvar keyboard-cat-overlay nil)
+(defvar keyboard-cat-overlay nil
+  "Invisibility overlay for `keyboard-cat-mode'.")
+
+(defvar keyboard-cat-step-function (lambda () 1)
+  "Function that the number of characters to \"emit\" at a time.
+This is a function so that a random number could be returned for
+each individual key event.")
 
 (defvar keyboard-cat-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map [remap self-insert-command] 'keyboard-cat-next)
     (define-key map [remap keyboard-quit] 'keyboard-cat-mode)
-    map))
+    map)
+  "Keymap for `keyboard-cat-mode'.")
 
 (define-minor-mode keyboard-cat-mode
-  "keyboard cat mode"
+  "keyboard cat mode: Clear the buffer and set the keymap to re-type the buffer on all input. Gives the illusion that you're a fast typist."
   :init nil
   :keymap keyboard-cat-mode-map
   (if keyboard-cat-mode
