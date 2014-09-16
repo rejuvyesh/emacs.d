174a9fe7e812c55b43730edda55b3b5febd0a34c
rejuvyesh <mail@rejuvyesh.com>
2014-09-16 20:49:54 +0530

    add paths to list

--- init/setup-load-path.el
+++ init/setup-load-path.el
@@ -6,8 +6,11 @@
       (progn
         (setq load-path (cons my-lisp-dir load-path))
         (normal-top-level-add-subdirs-to-load-path))))
-(setq load-path (cons (expand-file-name "~/.emacs.d/init")   load-path))
-(setq load-path (cons (expand-file-name "~/.emacs.d/themes") load-path))
+;; init has the startup scripts
+(setq load-path (cons (expand-file-name "~/.emacs.d/init") load-path))
+
+;; manual paths
+(add-to-list 'load-path "~/.emacs.d/themes") ; themes
 
 ;; package-repositories
 ;; no need for others when you have melpa
