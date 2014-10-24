cc07b11008ebcebefc461d93e09b31a0ba094946
rejuvyesh <mail@rejuvyesh.com>
2014-10-24 17:31:25 +0530

    pkg update

--- elpa/noflet-20140822.837/noflet-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "noflet" "20140822.837" "locally override functions" 'nil :url "https://github.com/nicferrier/emacs-noflet" :keywords '("lisp"))
---
+++ elpa/noflet-20141023.856/noflet-pkg.el
@@ -0,0 +1,1 @@
+(define-package "noflet" "20141023.856" "locally override functions" 'nil :url "https://github.com/nicferrier/emacs-noflet" :keywords '("lisp"))
