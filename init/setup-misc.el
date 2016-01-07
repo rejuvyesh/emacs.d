f5ded0f21719f70085c9b3cced55dc713fd9533a
rejuvyesh <mail@rejuvyesh.com>
2016-01-07 15:55:41 -0800

    safety problems

--- init/setup-misc.el
+++ init/setup-misc.el
@@ -8,6 +8,14 @@
 (setq shell-command-switch "-lc")
 
 ;; don't keep asking me about utf8
-(setq safe-local-variable-values '((encoding . utf-8)))
+(setq safe-local-variable-values '(
+                                   (encoding . utf-8)
+                                   (truncate-lines 1)
+                                   (hl-line-mode 1)
+                                   ))
+
+;; make sure we always know what's happening when eval-ing things
+(setq eval-expression-print-level nil)
+
 
 (provide 'setup-misc)
