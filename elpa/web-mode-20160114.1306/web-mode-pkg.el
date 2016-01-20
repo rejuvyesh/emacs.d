e7340d1085086671341dfc39cae6c423d4923d34
rejuvyesh <mail@rejuvyesh.com>
2016-01-19 19:39:45 -0800

    pkg update

---
+++ elpa/web-mode-20160114.1306/web-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "web-mode" "20160114.1306" "major mode for editing web templates" 'nil :stars '(#("853" 0 3 (font-lock-face paradox-star-face))) :url "http://web-mode.org" :keywords '("languages"))
