dec9a12cd157e1d48271142e53c4f3e63438bfd2
rejuvyesh <mail@rejuvyesh.com>
2016-02-09 21:20:21 -0800

    fix package setup

--- init/setup-packages.el
+++ init/setup-packages.el
@@ -1,15 +1,21 @@
 ;;; local packages
 ;; elpa package-repositories
+(setq package--init-file-ensured t)
+(setq package-enable-at-startup nil)
+
 (require 'package)
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
 ;; Install use-package if not there
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+(setq use-package-verbose t)
 (require 'use-package)
 
 (provide 'setup-packages)
