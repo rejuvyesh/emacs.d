bf0f19e4f58c23ace9b6bacd02ddef84786feaa8
rejuvyesh <mail@rejuvyesh.com>
2016-02-26 01:23:29 -0800

    Derp

--- init/setup-project.el
+++ init/setup-project.el
@@ -3,12 +3,11 @@
   :diminish projectile-mode
   :init
   (setq projectile-keymap-prefix (kbd "M-p"))
+  (projectile-global-mode)
   :config
   (setq projectile-file-exists-remote-cache-expire nil)
   (setq projectile-completion-system 'helm)
   (setq projectile-known-projects-file (emacs-d "cache/projectile-bookmarks.eld"))
-  (define-key projectile-command-map (kbd "s") 'projectile-ag)
-  (projectile-global-mode)
-  )
+  (define-key projectile-command-map (kbd "s") 'projectile-ag))
 
 (provide 'setup-project)
