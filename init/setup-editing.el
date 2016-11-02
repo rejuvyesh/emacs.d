4f0491be40a47d4d2f4fbd6287f8f3ba2e643528
rejuvyesh <mail@rejuvyesh.com>
2016-11-01 17:41:04 -0700

    sudo edit
    So many times I accidentally forget opening using sudo. I thought I had
    added a fix.

--- init/setup-editing.el
+++ init/setup-editing.el
@@ -721,4 +721,16 @@ to an html file in `user-emacs-directory'."
 (use-package keyboard-cat-mode
   :commands (keyboard-cat-mode))
 
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
 (provide 'setup-editing)
